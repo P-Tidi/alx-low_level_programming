2ae2d9318fb7b82387e84a48aa58e6df8c7876b0
P-Tidi <tidipaulinestudentin@outlook.com>
2023-06-21 06:38:30 -0700

    a function that prints the n times table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,13 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char);
+/*
+ * File: main.h
+ *
+ * Desc: Header file containing declartions for all function
+ */
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -16,3 +22,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 
 #endif
+
