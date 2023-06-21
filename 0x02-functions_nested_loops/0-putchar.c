aaf814fa966901f7e54a4925ad0f5961116d08c8
P-Tidi <tidipaulinestudentin@outlook.com>
2023-06-21 00:32:25 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * main - Entry point
- * Description:is written by mbah
- * Return: 0
+ * Description: 'print _putchar to stdout'
+ * Return: always 0
  */
 int main(void)
 {
