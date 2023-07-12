2ff03a117bb5fcc9bd66d6d75d07e44d70097fdd
P-Tidi <tidipaulinestudentin@outlook.com>
2023-07-12 01:56:27 -0700

    3-islower.c

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,16 +1,16 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 /**
- * _islower - check the code for Holberton School students.
- * @c:  is a parameter
- *  Return: Always 0.
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-	return (0);
+	return (c >= 'a' && c <= 'z');
 }
