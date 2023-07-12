fce5909817a09d506c1e3cc17fc59c2e6c577288
P-Tidi <tidipaulinestudentin@outlook.com>
2023-07-12 01:35:06 -0700

    _putchar

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,12 +1,10 @@
-#include <unistd.h>
-
-/**
- * _putchar - Writes a character to the standard output
- * @c: The character to be written
- * Return: On success, returns the character written. On error, -1 is returned.
- */
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	return putchar(c);
 }
